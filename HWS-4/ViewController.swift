8938e73e05233b2b4f874d4728c105b229736997
Zhenjiang Liu <zliu@kpd-i.com>
2016-09-10 17:33:43 -0400

    Initial Commit

---
+++ HWS-4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HWS-4
+//
+//  Created by ZhongZhongzhong on 2016-09-10.
+//  Copyright © 2016 ZhongZhongzhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
